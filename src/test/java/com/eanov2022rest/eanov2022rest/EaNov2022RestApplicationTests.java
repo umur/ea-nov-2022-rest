f9ded86ac3a9fab5476496389ac1538d404213d6
crisnaxtha <krishnashrestha49@gmail.com>
2022-11-01 13:55:11 -0500

    Inital Commit

---
+++ src/test/java/com/eanov2022rest/eanov2022rest/EaNov2022RestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eanov2022rest.eanov2022rest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EaNov2022RestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
