aaf91b81d79a6b8cb9702adb7a396f3b6be49ab4
vishwa <vishwadeep.ghimire@gmail.com>
2022-11-02 14:09:49 -0500

    Controller class refator

--- src/main/java/com/example/rest/controllers/CourseController.java
+++ src/main/java/com/example/rest/controllers/CourseController.java
@@ -1,7 +1,7 @@
 package com.example.rest.controllers;
 
 import com.example.rest.entities.Course;
-import com.example.rest.repositories.CourseRepository;
+import com.example.rest.services.CourseService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -12,30 +12,30 @@ import org.springframework.web.bind.annotation.*;
 public class CourseController {
 
     @Autowired
-    CourseRepository courseRepository;
+    CourseService courseService;
 
     @PostMapping
     ResponseEntity<?> create(@RequestBody Course course) {
-        return new ResponseEntity<>(courseRepository.save(course), HttpStatus.OK);
+        return new ResponseEntity<>(courseService.create(course), HttpStatus.OK);
     }
 
     @GetMapping
     ResponseEntity<?> getAll() {
-        return new ResponseEntity<>(courseRepository.getAll(), HttpStatus.OK);
+        return new ResponseEntity<>(courseService.getAll(), HttpStatus.OK);
     }
 
     @PutMapping("/{id}")
     ResponseEntity<?> update(@PathVariable("id") int id, @RequestBody Course course) {
-        return new ResponseEntity<>(courseRepository.update(id, course), HttpStatus.OK);
+        return new ResponseEntity<>(courseService.update(id, course), HttpStatus.OK);
     }
 
     @DeleteMapping("/{id}")
     ResponseEntity<?> delete(@PathVariable("id") int id) {
-        return new ResponseEntity<>(courseRepository.delete(id), HttpStatus.OK);
+        return new ResponseEntity<>(courseService.delete(id), HttpStatus.OK);
     }
 
     @GetMapping("/{id}")
     ResponseEntity<?> getById(@PathVariable("id") int id) {
-        return new ResponseEntity<>(courseRepository.getById(id), HttpStatus.OK);
+        return new ResponseEntity<>(courseService.getById(id), HttpStatus.OK);
     }
 }
