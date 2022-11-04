9cf633a56041833c725d10c77c0677ea6a4e24c8
JusticeManu <44110484+JusticeManu@users.noreply.github.com>
2022-11-03 21:12:00 -0600

    (chore) la2 Controller/entity/services done

---
+++ lab2/lab2/src/main/java/com/example/lab2/entity/product/Product.java
@@ -0,0 +1,9 @@
+package com.example.lab2.entity.product;
+
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+
+import javax.persistence.Entity;
+
+@Entity
+public class Product {
+}
